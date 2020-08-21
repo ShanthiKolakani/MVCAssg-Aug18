090e8b53b976759c989df8e92ee7e17b3f7b524b
Shanthi Kolakani <shanthi.kolakani@zensar.com>
2020-08-21 22:34:13 +0530

    add new file to master

---
+++ MVCProduct/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using MVCProduct.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProduct.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        
+        public ActionResult Index()
+        {
+            var product = GetProductsDeatils();
+
+
+            return View(product);
+        }
+
+
+        public IEnumerable<Product> GetProductsDeatils()
+        {
+            return new List<Product>
+            {
+                new Product{ProductId=101 ,ProductName="Ac",Rate=45000},
+                new Product{ProductId=102 ,ProductName="Mobile",Rate=38000},
+                new Product{ProductId=103 ,ProductName="Bike",Rate=94000}
+            };
+        }
+        public ActionResult Deatils(int id)
+        {
+            var product = GetProductsDeatils().SingleOrDefault(c => c.ProductId == id);
+            if (product == null)
+                return HttpNotFound();
+            return View(product);
+        }
+    }
+}
